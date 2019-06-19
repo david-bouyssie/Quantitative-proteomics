6c578a63c1db44cab1cffd48713dc26a59625fd1
CoralieLudwig <37569238+CoralieLudwig@users.noreply.github.com>
2019-06-19 15:09:32 +0200

    Add files via upload

--- Launchers/run.bat
+++ Launchers/run.bat
@@ -1,6 +1,3 @@
-::@echo off
-::set PATH=%PATH%;C:/Program Files/R/R-3.5.1/bin
-
 
 :: ###################
 :: # Parsing modules #
@@ -8,7 +5,7 @@
 
 :: Command line format : Rscript --vanilla run_parsing.R [file to parse] [output] [parser option : Proline/Maxquant/ProteomeDiscoverer] [abundance type option : LFQ/iBAQ/raw_abundance/abundance] [if xlsx file, sheet number]
 :: Example : 
-::Rscript --vanilla run_parsing.R "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/Quanti/Raw/Proline/Sandrine/180619_PCP Analysis from DEAE sample inj 1 SLB_18-06-2019_0931 (1).xlsx" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.txt" Proline abundance 4
+::Rscript --vanilla run_parsing.R "path_to_file_to_parse/name_of_file_to_parse" "path_to_desired_output_directory/desired_name" Proline abundance 4
 
 
 :: ###############################
@@ -17,7 +14,7 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R QC [input (standardized format)] [output] [normalization option: TRUE/FALSE] [delete empty lines : TRUE/FALSE]
 :: Example :
-::Rscript --vanilla run_proteomics_stats.R QC "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.txt" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.html" TRUE FALSE
+::Rscript --vanilla run_proteomics_stats.R QC "../../Example/Datasets/Parsed_proteins_set.txt" "../../Example/Reports/QC_example.html" TRUE FALSE
 
 
 :: ################################
@@ -26,7 +23,7 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input (standardized format)] [report output] [table output] [experimental design file] [parameters file]
 :: Example :
-::Rscript --vanilla run_proteomics_stats.R DA "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/Quanti/Parsed_data/Human_yeast/all_mrf_withsamplename.txt" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.html" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.txt" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/Quanti/Parsed_data/Human_yeast/designExp.txt" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/Quanti/Parsed_data/Human_yeast/Params_da.txt"
+Rscript --vanilla run_proteomics_stats.R DA "../../Example/Datasets/Parsed_proteins_set.txt" "../../Example/Reports/DA_example.html" "../../Example/Reports/DA_example.txt" "../../Example/Datasets/designExp.txt" "../../Example/Datasets/Parameters.txt"
 
 
 :: ##############
@@ -35,7 +32,7 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input folder] [variant list] [output report] [number of tests] [min ratio] [max ratio] [default ratio] [min pval] [max pval] [default pval]
 :: Example :
-::Rscript --vanilla run_ROC.R "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Results/Human_yeast/ROC/imputation/gaussian" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Results/Human_yeast/ROC/variants.csv" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/test.html" 10 0.5 10 0.58 0 0.05 0.05
+Rscript --vanilla run_ROC.R "../../Example/Datasets/ROC/" "../../Example/Datasets/ROC/variants.csv" "../../Example/Reports/ROC_example.html" 10 0.5 10 0.58 0 0.05 0.05
 
 
 :: ############
@@ -44,4 +41,4 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input] [output folder]
 :: Example :
-Rscript --vanilla run_cytoc.R "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/CytoC/Rapport/cytoc_xics_test.csv" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/"
+::Rscript --vanilla run_cytoc.R "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/CytoC/Rapport/cytoc_xics_test.csv" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/"
