7e6543dd642b75b58e41e6da051487556255e450
CoralieLudwig <37569238+CoralieLudwig@users.noreply.github.com>
2019-06-19 15:18:57 +0200

    Add files via upload

--- Launchers/run.bat
+++ Launchers/run.bat
@@ -14,7 +14,7 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R QC [input (standardized format)] [output] [normalization option: TRUE/FALSE] [delete empty lines : TRUE/FALSE]
 :: Example :
-::Rscript --vanilla run_proteomics_stats.R QC "../../Example/Datasets/Parsed_proteins_set.txt" "../../Example/Reports/QC_example.html" TRUE FALSE
+::Rscript --vanilla run_proteomics_stats.R QC "../../Example/Datasets/QC-DA/Parsed_proteins_set.txt" "../../Example/Reports/QC_example.html" TRUE FALSE
 
 
 :: ################################
@@ -23,7 +23,7 @@
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input (standardized format)] [report output] [table output] [experimental design file] [parameters file]
 :: Example :
-Rscript --vanilla run_proteomics_stats.R DA "../../Example/Datasets/Parsed_proteins_set.txt" "../../Example/Reports/DA_example.html" "../../Example/Reports/DA_example.txt" "../../Example/Datasets/designExp.txt" "../../Example/Datasets/Parameters.txt"
+::Rscript --vanilla run_proteomics_stats.R DA "../../Example/Datasets/QC-DA/Parsed_proteins_set.txt" "../../Example/Reports/DA_example.html" "../../Example/Reports/DA_example.txt" "../../Example/Datasets/QC-DA/designExp.txt" "../../Example/Datasets/QC-DA/Parameters.txt"
 
 
 :: ##############
@@ -32,7 +32,7 @@ Rscript --vanilla run_proteomics_stats.R DA "../../Example/Datasets/Parsed_prote
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input folder] [variant list] [output report] [number of tests] [min ratio] [max ratio] [default ratio] [min pval] [max pval] [default pval]
 :: Example :
-Rscript --vanilla run_ROC.R "../../Example/Datasets/ROC/" "../../Example/Datasets/ROC/variants.csv" "../../Example/Reports/ROC_example.html" 10 0.5 10 0.58 0 0.05 0.05
+::Rscript --vanilla run_ROC.R "../../Example/Datasets/ROC/" "../../Example/Datasets/ROC/variants/variants.csv" "../../Example/Reports/ROC_example.html" 10 0.5 10 0.58 0 0.05 0.05
 
 
 :: ############
@@ -41,4 +41,4 @@ Rscript --vanilla run_ROC.R "../../Example/Datasets/ROC/" "../../Example/Dataset
 
 :: Command line format : Rscript --vanilla run_proteomics_stats.R DA [input] [output folder]
 :: Example :
-::Rscript --vanilla run_cytoc.R "D:/Utilisateurs/cludwig/stage/RStudio/Tests/Data/CytoC/Rapport/cytoc_xics_test.csv" "D:/Utilisateurs/cludwig/stage/RStudio/Tests/"
+::Rscript --vanilla run_cytoc.R "../../Example/Datasets/CytoC/cytoc_xics_test.csv" "../../Example/Reports"
